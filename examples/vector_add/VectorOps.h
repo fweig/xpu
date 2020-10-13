9bb3c0e499fcd357050062b45ce476a8b0fef7d7
Felix Weiglhofer <felix.weiglhofer@gmail.com>
2020-10-13 14:30:46 +0000

    Add cmake function to add device libraries.

--- examples/vector_add/VectorOps.h
+++
@@ -1,3 +0,0 @@
-#define XPU_DEVICE_LIBRARY_NAME VectorOps
-#define XPU_DEVICE_LIBRARY_KERNEL_DEF_FILE "../../../../examples/vector_add/kernels.h"
-#include <xpu/device_library/template/frontend.h>
--- examples/vector_add/VectorOpsCPU.cpp
+++
@@ -1,9 +0,0 @@
-#define XPU_DEVICE_LIBRARY_BACKEND_NAME VectorOpsCPU
-#define XPU_DEVICE_LIBRARY_FRONTEND_H "../../../../examples/vector_add/VectorOps.h"
-
-#include <xpu/device_library/template/backend.h>
-
-// TODO include singleton...
-#include <xpu/device_library/template/frontend.cpp>
-
-#include "vec_add.cpp"

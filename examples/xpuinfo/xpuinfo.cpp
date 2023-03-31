f334ba8fa2b901b6d3dffe482532c55c12c71233
Felix Weiglhofer <weiglhofer@fias.uni-frankfurt.de>
2023-03-31 14:40:55 +0000

    Add verbose-mode to xpuinfo.

--- examples/xpuinfo/xpuinfo.cpp
+++ examples/xpuinfo/xpuinfo.cpp
@@ -2,20 +2,41 @@
 
 #include <cstdio>
 
-int main() {
+int main(int argc, char **argv) {
     xpu::initialize();
+
+    bool verbose = false;
+
+    if (argc > 1 && std::string(argv[1]) == "-v")
+        verbose = true;
 
     std::vector<xpu::device> devices = xpu::device::all();
 
     for (auto &d : devices) {
         xpu::device_prop prop{d};
 
-        std::cout << prop.xpuid() << ": " << prop.name();
+        if (!verbose) {
+            std::cout << prop.xpuid() << ": " << prop.name();
 
-        if (!prop.arch().empty())
-            std::cout << " [" << prop.arch() << "]";
+            if (!prop.arch().empty())
+                std::cout << " [" << prop.arch() << "]";
 
-        std::cout << std::endl;
+            std::cout << std::endl;
+
+            continue;
+        }
+
+        std::cout << "Device: " << prop.xpuid() << std::endl;
+        std::cout << "  Name: " << prop.name() << std::endl;
+        std::cout << "  Driver: " << prop.backend() << std::endl;
+        std::cout << "  Architecture: " << prop.arch() << std::endl;
+        std::cout << "  Shared memory size: " << prop.shared_mem_size() << std::endl;
+        std::cout << "  Constant memory size: " << prop.const_mem_size() << std::endl;
+        std::cout << "  Warp size: " << prop.warp_size() << std::endl;
+        std::cout << "  Max threads per block: " << prop.max_threads_per_block() << std::endl;
+        std::cout << "  Max grid size: " << prop.max_grid_size()[0] << "x" << prop.max_grid_size()[1] << "x" << prop.max_grid_size()[2] << std::endl;
+        std::cout << "  Global memory total: " << prop.global_mem_total() << std::endl;
+        std::cout << "  Global memory available: " << prop.global_mem_available() << std::endl;
     }
 
     return 0;
